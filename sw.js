5fd279c150403b85601b0420241a8c15273cec3a
jaebinpark <pyg1986@gmail.com>
2025-08-25 22:51:58 +0900

    fresh: v7 clean reinstall (merged base + anti v3, icon fix, SW cache-bust, global bridge)

--- sw.js
+++ sw.js
@@ -22,4 +22,5 @@ self.addEventListener('fetch', e=>{
     e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request).then(res=>{const copy=res.clone();caches.open('core-'+CACHE_VERSION).then(c=>c.put(e.request,copy)).catch(()=>{});return res;}).catch(()=>caches.match('./index.html'))));
   }else e.respondWith(fetch(e.request));
 });
-// cache-bust 2025-08-25T19:25:06
+
+// cache-bust 2025-08-25T13:25:59.294077
